77a53e186e63fdb0c6aa8ea57e196e15158f56f8
Caio-Santana <caio.santana@live.com>
2023-05-20 14:00:10 -0300

    Add Spring MVC Controller Advice

--- src/test/java/guru/springframework/spring5recipeapp/controllers/ImageControllerTest.java
+++ src/test/java/guru/springframework/spring5recipeapp/controllers/ImageControllerTest.java
@@ -36,7 +36,9 @@ class ImageControllerTest {
         MockitoAnnotations.openMocks(this);
 
         imageController = new ImageController(imageService, recipeService);
-        mockMvc = MockMvcBuilders.standaloneSetup(imageController).build();
+        mockMvc = MockMvcBuilders.standaloneSetup(imageController)
+                .setControllerAdvice(new ControllerExceptionHandler())
+                .build();
     }
 
     @Test
@@ -97,5 +99,13 @@ class ImageControllerTest {
         byte[] responseBytes = response.getContentAsByteArray();
 
         assertEquals(s.getBytes().length, responseBytes.length);
+    }
+
+    @Test
+    public void testGetImageNumberFormatException() throws Exception {
+
+        mockMvc.perform(get("/recipe/asdf/recipeimage"))
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("400error"));
     }
 }
